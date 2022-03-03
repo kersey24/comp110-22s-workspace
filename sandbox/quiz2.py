4a880c98eaad08ef425405e0d220025a08a1d693
kersey24@ad.unc.edu <kersey24@ad.unc.edu>
2022-03-02 21:49:24 -0500

    quiz practice

--- sandbox/quiz2.py
+++ sandbox/quiz2.py
@@ -49,20 +49,31 @@ def odd_and_even(a: list[int]) -> list[int]:
 def vowels_and_threes(a: str) -> str:
     result: str = ""
     vowel: list[str] = ["a", "e", "i", "o", "u"]
-    is_vowel: bool = False
+    # is_vowel: bool = False
     i: int = 0
     while i < len(a): 
-        is_vowel = False
-        j: int = 0
-        while j < len(a):
-            if vowel[j] == a[i]:
-                is_vowel = True
-            j += 1 
-        if i % 3 == 0 and is_vowel: 
+        # is_vowel = False
+        # j: int = 0
+        # # while j < len(a):
+        # #     if vowel[j] == a[i]:
+        # #         is_vowel = True
+        # #     j += 1 
+        if i % 3 == 0 and a[i] in vowel: 
             result += ""
         elif i % 3 == 0: 
             result += a[i]
-        elif is_vowel: 
+        elif a[i] in vowel: 
             result += a[i]
         i += 1
     return result
+
+
+def average_grades(grades: dict[str, list[int]]) -> dict[str, float]:
+    """maps each students grades to their average."""
+    average: dict[str, float] = dict()
+    for students in grades: 
+        total: int = 0 
+        for all in grades[students]: 
+            total += all
+        average[students] = total / len(grades[students])
+    return average
